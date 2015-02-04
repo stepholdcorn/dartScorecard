579dbb616defb8480d4944e3ba0c2d8e6de701f0
stepholdcorn <stephieoldcorn@gmail.com>
2015-02-04 09:25:32 +0000

    Creates the turn score from the throw of three darts

--- js/scoreboard.js
+++ js/scoreboard.js
@@ -1,9 +1,14 @@
 function Scoreboard() {
   this.total = 501;
+  this.turnScore = undefined;
 }
 
 Scoreboard.prototype.subtractTurnScore = function(turnScore) {
   this.total -= turnScore;
+};
+
+Scoreboard.prototype.calculateTurnScore = function(dart1, dart2, dart3) {
+  this.turnScore = dart1 + dart2 + dart3;
 };
 
 module.exports = Scoreboard;
--- spec/scoreboardSpec.js
+++ spec/scoreboardSpec.js
@@ -2,15 +2,28 @@ var Scoreboard = require('../js/scoreboard')
 
 describe('scoreboard', function() {
 
+  var scoreboard;
+
+  beforeEach(function() {
+    scoreboard = new Scoreboard();
+  });
+
   it('starts with a score of 501', function() {
-    var scoreboard = new Scoreboard();
     expect(scoreboard.total).toEqual(501);
   });
 
   it('subtracts the score of three darts', function() {
-    var scoreboard = new Scoreboard();
     scoreboard.subtractTurnScore(3);
     expect(scoreboard.total).toEqual(498);
+  });
+
+  it('creates the turn score from throwing three darts', function() {
+    scoreboard.calculateTurnScore(1, 1, 1);
+    expect(scoreboard.turnScore).toEqual(3);
+  });
+
+  it('ignores a turn score that is greater than the total score', function() {
+    expect(scoreboard.total).toEqual(50);
   });
 
 });
